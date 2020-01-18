8ec943d763163824c0f957b4f3ed7cf31a02d5ec
Michael Bowman <micbowman25@gmail.com>
2020-01-18 15:36:15 -0600

    working questions

--- carselect.js
+++ carselect.js
@@ -7,29 +7,36 @@ var questions = [
 
     {
         title: "What is the submodel?",
-        choice: [""]
+        choice: ["stuff", "more stuff", "other stuff"]
     }
 ]
 
 var questionOne = "";
 var currentQuestion = 1;
-var questionTwo = "";
 
-for(i=0; i< 2; i++){
+getQuestion = () => {
+    $("#questions").html("");
+    $("#choices").html("");
 
+    let currentChoices = questions[currentQuestion].choice;
 
+    $("#questions").html(questions[currentQuestion].title);
+
+    for(let i = 0; i<currentChoices.length; i++) {
+        let qButton = $("<button>").text(currentChoices[i]);
+        qButton.addClass("choices");
+        qButton.attr("type", "submit");
+        qButton.attr("value", currentChoices[i]);
+        $("#choices").append(qButton);
+    }
+    currentQuestion++;
+}
 
 questionOne += "<p>" + questions[0].title + "</p>";
 for(let i = 0; i < questions[0].choice.length; i++) {
-    questionOne += "<input type=button class=choices value=" + questions[0].choice[i] + ">";
+    questionOne += "<button type=submit class=choices value=" + questions[0].choice[i] + ">" + questions[0].choice[i] + "</button>";
 };
 
-document.getElementById("questions").innerHTML = questionOne;
+$("#questions").html(questionOne);
 
-questionTwo += "<p>" + questions[1].title + "</p>";
-for(let i = 0; i < questions[1].choice.length; i++) {
-    questionTwo += "<input type=button class=choices value=" + questions[0].choice[i] + ">";
-};
-
-document.getElementById("questions").innerHTML = questionTwo;
-}
+$(document).on("click", ".choices", getQuestion);
