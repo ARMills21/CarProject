5b333d382655d36c6013ff38f9637f1900c4049d
Michael Bowman <micbowman25@gmail.com>
2020-01-19 00:48:27 -0600

    add functions for question functionality

--- carselect.js
+++ carselect.js
@@ -1,18 +1,12 @@
 var questions = [
     
     {
-        title: "What is the car type?",
-        choice: ["Car", "Truck", "SUV", "Van", "Minivan"]
-    },
-
-    {
-        title: "What is the submodel?",
+        title: "What is the car type submodel?",
         choice: ["4dr Hatchback", "Cargo Minivan", "Coup",
     "Passenger Van", "2dr Hatchback", "Cargo Van", "Crew Cab Pickup", "Regular Cap Pickup",
     "2dr SUV", "Convertible", "Extended Cab Pickup", "Sedan", "4dr SUV", "Convertible SUV",
     "Passenger Minivan", "Wagon"]
     },
-    
     {
         title: "What Size of vehicle?",
         choice: ["Compact", "Midsize", "Large"]
@@ -29,32 +23,60 @@ var questions = [
     }
 ]
 
-var questionOne = "";
 var currentQuestion = 1;
+var userChoices = [];
 
-getQuestion = () => {
-    $("#questions").html("");
-    $("#choices").html("");
-
-    let currentChoices = questions[currentQuestion].choice;
-
-    $("#questions").html(questions[currentQuestion].title);
-
-    for(let i = 0; i<currentChoices.length; i++) {
-        let qButton = $("<button>").text(currentChoices[i]);
-        qButton.addClass("choices");
-        qButton.attr("type", "submit");
-        qButton.attr("value", currentChoices[i]);
-        $("#choices").append(qButton);
-    }
-    currentQuestion++;
-}
-
-questionOne += "<p>" + questions[0].title + "</p>";
-for(let i = 0; i < questions[0].choice.length; i++) {
-    questionOne += "<button type=submit class=choices value=" + questions[0].choice[i] + ">" + questions[0].choice[i] + "</button>";
+getFirstQuestion = () => {
+    const firstChoices = questions[0].choice;
+    $("#questions").html(questions[0].title);
+    for(let i=0;i<firstChoices.length; i++) {
+        let qOption = $("<option>").text(firstChoices[i]);
+        qOption.addClass("choices");
+        qOption.attr("value", firstChoices[i]);
+        $("#choices").append(qOption);
+    };
 };
 
-$("#questions").html(questionOne);
+getQuestion = event => {
+    event.preventDefault();
 
-$(document).on("click", ".choices", getQuestion);
+    const userInput = $("#choices option:selected").val();
+    getUserChoice(userInput);
+
+    if(currentQuestion >= 4) {
+        getResults(userChoices);
+    }
+    else {
+        $("#questions").html("");
+        $("#choices").html("");
+
+        let currentChoices = questions[currentQuestion].choice;
+
+        $("#questions").html(questions[currentQuestion].title);
+
+        for(let i = 0; i<currentChoices.length; i++) {
+            let qOption = $("<option>").text(currentChoices[i]);
+            qOption.addClass("choices");
+            qOption.attr("value", currentChoices[i]);
+            $("#choices").append(qOption);
+        }
+        currentQuestion++;
+    }
+}
+
+getAPI = Arr => {
+    
+}
+
+getUserChoice = choice => {
+    userChoices.push(choice);
+    console.log(userChoices);
+};
+
+getResults = choiceArr => {
+    window.location.href = "results.html";
+    getAPI (choiceArr);
+};
+
+getFirstQuestion();
+$(document).on("click", "#nextBtn", getQuestion);
